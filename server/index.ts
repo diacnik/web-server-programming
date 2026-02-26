b7ad3f20d34160807b627b7fbe3f3cb6a4479de6
Ian Kincaid <i.s.kincaid@gmail.com>
2026-02-26 15:18:02 -0500

    Added express

---
+++ server/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const PORT = 3000;
+const SERVER = `localhost`;
+
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.send("Hello, World!");
+}).get("/suny", (_req, res) => {
+  res.send("The best plan of my life!");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://${SERVER}:${PORT}`);
+});
